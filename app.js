3f27149ca8dbf51e231db1133bebbe8d150e1eb5
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-02-14 18:09:35 +0530

    changed the PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 const userRouter = require('./routes/userRouter')
 const adminRouter = require('./routes/adminRouter')
 const mongoose = require('mongoose')
