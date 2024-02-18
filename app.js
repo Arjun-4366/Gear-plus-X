71e996f18e1cd1d60a6deb817e23ec8fbe2a9279
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-02-18 15:39:01 +0530

    fixed deployement issue

--- app.js
+++ app.js
@@ -13,39 +13,7 @@ app.use('/public',express.static('public'))
 
 
 
-// Create a Secrets Manager client
-const client = new AWS.SecretsManager();
 
-// Define parameters for retrieving the secret
-const params = {
-  SecretId: 'secrets'
-};
-
-// Retrieve the secret value
-client.getSecretValue(params, (err, data) => {
-  if (err) {
-    console.error("Error retrieving secret:", err);
-  } else {
-    // Parse the secret JSON string
-    const secret = JSON.parse(data.SecretString);
-
-    // Set the environment variables
-    process.env.PORT = secret.PORT;
-    process.env.HOST = secret.HOST;
-    process.env.SMPT_MAI = secret.SMPT_MAI;
-    process.env.SMPT_PASSWORD = secret.SMPT_PASSWORD;
-    process.env.SECRET = secret.SECRET;
-    process.env.ADMIN_SECRET = secret.ADMIN_SECRET;
-    process.env.RAZORPAY_KEY_ID = secret.RAZORPAY_KEY_ID;
-    process.env.RAZORPAY_SECRET_KEY = secret.RAZORPAY_SECRET_KEY;
-    process.env.CLLIENT_ID = secret.CLLIENT_ID;
-    process.env.CLLIENT_SECRET = secret.CLLIENT_SECRET;
-    process.env.MONGO_URL = secret.MONGO_URL;
-
-    // Start your application
-    // Your application code here
-  }
-});
 
 const PORT = process.env.PORT || 4000
 
